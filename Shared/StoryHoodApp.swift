c4ec4161e500573a9c1b83c016d2d70da47b0d1d
Tales Conrado <talesmateuscc@gmail.com>
2021-05-20 09:30:29 -0300

    adds message buble

--- Shared/StoryHoodApp.swift
+++ Shared/StoryHoodApp.swift
@@ -10,11 +10,17 @@ import SwiftUI
 @main
 struct StoryHoodApp: App {
     let persistenceController = PersistenceController.shared
+    let messages: [Message] = [Message(type: .character, text: "Oi, eu sou o Lobo Bad Lobo huhuhuh", id: 1, image: nil, isLastMessage: false),
+                               Message(type: .reader, text: "Olá e eu não sou mal não", id: 2, image: nil, isLastMessage: false),
+                               Message(type: .character, text: "ta certo entao seu mentiroso vc vai ver eu vou te pegar e vc vai virar um porquinho q constroi casa de palha e ai eu vou coisa e vc vai ver", id: 3, image: nil, isLastMessage: false)]
 
     var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//        }
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            MessagesListView(messages: messages)
         }
     }
 }
