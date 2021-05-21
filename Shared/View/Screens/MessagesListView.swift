57bbd56ee78ed5a4ccdf11145ef5bc15b463d465
Tales Conrado <talesmateuscc@gmail.com>
2021-05-21 10:50:26 -0300

    adds timer for messages changes

--- Shared/View/Screens/MessagesListView.swift
+++ Shared/View/Screens/MessagesListView.swift
@@ -10,121 +10,38 @@ import SwiftUI
 struct MessagesListView: View {
 
     let messages: [Message]
+    @State private var lastSeenMessageId: Int = -1
+    @State private var onScreenMessages: [Message] = []
+    @State private var isTyping = false
+    let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
 
     var body: some View {
         VStack {
+            MessagesTopBar(isTyping: $isTyping)
             ScrollView {
                 VStack {
-                    ForEach(messages, id: \.id) { message in
-                        ChatBubble(messageType: message.type) {
+                    ForEach(onScreenMessages, id: \.id) { message in
+                        MessageBubble(messageType: message.type) {
                             Text(message.text ?? "")
                         }
                     }
                 }
             }
-        }.background(Color(.displayP3, red: 252/255, green: 249/255, blue: 246/255, opacity: 1))
-    }
-}
-
-struct ChatBubble<Content>: View where Content: View {
-    let direction: ChatBubbleShape.Direction
-    let content: () -> Content
-    let readerColor = Color(.displayP3, red: 255/255, green: 236/255, blue: 208/255, opacity: 1)
-    let characterColor = Color(.displayP3, red: 255/255, green: 213/255, blue: 198/255, opacity: 1)
-
-    init(messageType: MessageType, @ViewBuilder content: @escaping () -> Content) {
-        self.content = content
-        self.direction = messageType == .reader ? .right : .left
-    }
-
-    var body: some View {
-        HStack {
-            if direction == .right {
-                Spacer()
-            }
-            content()
-                .padding(.all, 20)
-                .foregroundColor(Color.black)
-                .background(direction == .left ? characterColor :  readerColor)
-                .clipShape(ChatBubbleShape(direction: direction))
-
-            if direction == .left {
-                Spacer()
-            }
-        }.padding([(direction == .left) ? .leading : .trailing, .top, .bottom], 20)
-        .padding((direction == .right) ? .leading : .trailing, 150)
-    }
-}
-
-struct ChatBubbleShape: Shape {
-    enum Direction {
-        case left
-        case right
-    }
-
-    let direction: Direction
-
-    func path(in rect: CGRect) -> Path {
-        return (direction == .left) ? getLeftBubblePath(in: rect) : getRightBubblePath(in: rect)
-    }
-
-    private func getLeftBubblePath(in rect: CGRect) -> Path {
-        let width = rect.width
-        let height = rect.height
-        let path = Path { p in
-            p.move(to: CGPoint(x: 25, y: height))
-            p.addLine(to: CGPoint(x: width - 20, y: height))
-            p.addCurve(to: CGPoint(x: width, y: height - 20),
-                       control1: CGPoint(x: width - 8, y: height),
-                       control2: CGPoint(x: width, y: height - 8))
-            p.addLine(to: CGPoint(x: width, y: 20))
-            p.addCurve(to: CGPoint(x: width - 20, y: 0),
-                       control1: CGPoint(x: width, y: 8),
-                       control2: CGPoint(x: width - 8, y: 0))
-            p.addLine(to: CGPoint(x: 21, y: 0))
-            p.addCurve(to: CGPoint(x: 4, y: 20),
-                       control1: CGPoint(x: 12, y: 0),
-                       control2: CGPoint(x: 4, y: 8))
-            p.addLine(to: CGPoint(x: 4, y: height - 11))
-            p.addCurve(to: CGPoint(x: 0, y: height),
-                       control1: CGPoint(x: 4, y: height - 1),
-                       control2: CGPoint(x: 0, y: height))
-            p.addLine(to: CGPoint(x: -0.05, y: height - 0.01))
-            p.addCurve(to: CGPoint(x: 25, y: height),
-                       control1: CGPoint(x: 16, y: height),
-                       control2: CGPoint(x: 20, y: height))
-
         }
-        return path
+        .background(Color(.displayP3, red: 252/255, green: 249/255, blue: 246/255, opacity: 1))
+        .onReceive(timer, perform: { _ in
+            typingToggle()
+        })
     }
 
-    private func getRightBubblePath(in rect: CGRect) -> Path {
-        let width = rect.width
-        let height = rect.height
-        let path = Path { p in
-            p.move(to: CGPoint(x: 25, y: height))
-            p.addLine(to: CGPoint(x:  20, y: height))
-            p.addCurve(to: CGPoint(x: 0, y: height - 20),
-                       control1: CGPoint(x: 8, y: height),
-                       control2: CGPoint(x: 0, y: height - 8))
-            p.addLine(to: CGPoint(x: 0, y: 20))
-            p.addCurve(to: CGPoint(x: 20, y: 0),
-                       control1: CGPoint(x: 0, y: 8),
-                       control2: CGPoint(x: 8, y: 0))
-            p.addLine(to: CGPoint(x: width - 21, y: 0))
-            p.addCurve(to: CGPoint(x: width - 4, y: 20),
-                       control1: CGPoint(x: width - 12, y: 0),
-                       control2: CGPoint(x: width - 4, y: 8))
-            p.addLine(to: CGPoint(x: width - 4, y: height - 11))
-            p.addCurve(to: CGPoint(x: width, y: height),
-                       control1: CGPoint(x: width - 4, y: height - 1),
-                       control2: CGPoint(x: width, y: height))
-            p.addLine(to: CGPoint(x: width + 0.05, y: height - 0.01))
-            p.addCurve(to: CGPoint(x: width - 25, y: height),
-                       control1: CGPoint(x: width - 16, y: height),
-                       control2: CGPoint(x: width - 20, y: height))
-
+    private func typingToggle() {
+        if let nextMessage = messages.first(where: { !onScreenMessages.contains($0) }) {
+            if isTyping {
+                onScreenMessages.append(nextMessage)
+            }
+        } else {
+            isTyping.toggle()
         }
-        return path
+        isTyping.toggle()
     }
 }
