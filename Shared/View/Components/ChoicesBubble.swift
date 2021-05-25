2929d0a8ada564faab5a23e4437d3896502c0643
Kellyane Nogueira <kellyanenogueira1@gmail.com>
2021-05-24 22:09:33 -0300

    feedback para o usuário e função de digitar

--- Shared/View/Components/ChoicesBubble.swift
+++ Shared/View/Components/ChoicesBubble.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ChoicesView: View {
     @Binding var text: String
-    var choices: [Option] = [Option(text: "Você fez maravilhas, loucuras no meu coração. Um beijo para você, não posso demorar. Tô numa ligação urbana, tem mais gente pra ligar. Você fez maravilhas, loucuras no meu coração. Um beijo para você, não posso demorar.Você fez maravilhas, loucuras no meu coração. Um beijo para você, não posso demorar. Tô numa ligação urbana, tem mais gente pra ligar. Você fez maravilhas, loucuras no meu coração. Um beijo para você, não posso demorar.", id: 1), Option(text: "Um beijo para você, não posso demorar. Tô numa ligação urbana, tem mais gente pra ligar. Você fez maravilhas, loucuras no meu coração. Um beijo para você, não posso demorar.", id: 2), Option(text: "Você fez maravilhas, loucuras no meu coração. Um beijo para você, não posso demorar. Tô numa ligação urbana, tem mais gente pra ligar. Você fez maravilhas, loucuras no meu coração. Um beijo para você, não posso demorar.Você fez maravilhas, loucuras no meu coração. ", id: 3)]
+    var choices: [Option] = [Option(text: "Você fez maravilhas, loucuras no meu coração. Um beijo para você, não posso demorar. Tô numa ligação urbana, tem mais gente pra ligarVocê fez maravilhas, loucuras no meu coração. Um beijo para você, não posso demorar. Tô numa ligação urbana, tem mais gente pra ligar", id: 1), Option(text: "Você fez maravilhas, loucuras no meu coração. Um beijo para você, não posso demorar. Tô numa ligação urbana, tem mais gente pra ligar", id: 2), Option(text: "Você fez maravilhas, loucuras no meu coração. Um beijo para você, não posso demorar. Tô numa ligação urbana, tem mais gente pra ligar", id: 3)]
     
     @State var selectedOption: Option? = nil
     
@@ -18,12 +18,23 @@ struct ChoicesView: View {
             ForEach(choices, id: \.id) { option in
                 OptionBubble(isSelected: option == selectedOption, option: option)
                     .onTapGesture {
+    
                         selectedOption = option
-//                        var splitText = Array(option.text)
-//                        for i in splitText {
-//                            text += i
-//                        }
-                        text = option.text
+                        
+                        let subAnswer = Substring(option.text)
+                        var repetitions = 0
+                        
+                        Timer.scheduledTimer(withTimeInterval: 0.09, repeats: true) { [self] timer in
+                        
+                            repetitions+=1
+                            let range = subAnswer.index(subAnswer.startIndex, offsetBy: repetitions)
+                            
+                            text = String(subAnswer[..<range])
+                            
+                            if repetitions == option.text.count {
+                                timer.invalidate()
+                            }
+                        }
                     }
             }
         }
--- Shared/View/Screens/AnswerView.swift
+++ Shared/View/Screens/AnswerView.swift
@@ -16,22 +16,35 @@ struct AnswerView: View {
         Spacer()
         VStack {
             HStack {
-                Text(text) //Or TextEditor(text: $text)
-                    .lineLimit(3)
-                    .frame(maxWidth: 715, minHeight: 20, maxHeight: 65, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 6)
-                        .stroke(Color(0xCBC5C5), lineWidth: 2)
-                    )
-                    .background(Color.white)
-                    .cornerRadius(6)
-                    .onTapGesture {
-                        if !isEnabled {
+                if !isEnabled {
+                    Text("Escreva sua mensagem")
+                        .foregroundColor(.secondary)
+                        .lineLimit(1)
+                        .frame(width: 715, height: 35)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 17)
+                            .stroke(Color(0xCBC5C5), lineWidth: 2)
+                        )
+                        .background(Color.white)
+                        .cornerRadius(6)
+                        .onTapGesture {
                             let feedback = UINotificationFeedbackGenerator()
                             feedback.notificationOccurred(.error)
                         }
-                    }
+                    
+                } else {
+                    Text(text) //Or TextEditor(text: $text)
+                        .lineLimit(3)
+                        .frame(width: 715)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                            .stroke(Color(0xCBC5C5), lineWidth: 2)
+                        )
+                        .background(Color.white)
+                        .cornerRadius(6)
+                }
                 
                 Button(action: {
                     interactor.saveUserMessage(option: text)
@@ -40,14 +53,12 @@ struct AnswerView: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 }.frame(maxHeight: 20)
-            }
-            .padding()
+            }.padding()
             
             if isEnabled {
                 ChoicesView(text: $text)
             }
-        }
-        .background(Color(0xF6F2F2))
+        }.background(Color(0xF6F2F2))
     }
     
 }
